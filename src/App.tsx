d05e5c65776cca70aff05a2de7037409b15087b9
Giorgi Gvimradze <natceo@gmail.com>
2020-09-24 01:53:17 +0400

    first component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TimerDoughnut from './components/timer-doughnut/timer-doughnut'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TimerDoughnut/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/timer-doughnut/timer-doughnut.tsx
@@ -0,0 +1,67 @@
+import React, { useState,useEffect } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import './timer-doughnut.css'
+
+function RightBar(){
+
+  const sinav_suresi = 90;
+  //left time calculator
+  let finish = new Date();
+  finish.setSeconds(finish.getSeconds() + sinav_suresi);
+  const [endTime] = useState(finish);
+
+  const calculateTimeLeft = () => {
+    const difference = +endTime - +new Date();
+    if (difference>0)
+      return difference;
+    else {
+      return 0
+    }
+  }
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  const checkTimeFunction = () => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft);
+    },1000);
+  };
+
+// To-Do !! add dependency array
+  useEffect(() => {
+    checkTimeFunction()
+  });
+
+  //chart
+  const [chartData,setChartData] = useState({});
+
+  useEffect( () => {
+    if(timeLeft){
+    const chart = () => {
+      if(timeLeft){
+        setChartData({
+          datasets:
+          [
+            {
+              data:[timeLeft, sinav_suresi*1000 - timeLeft],
+              backgroundColor:['#61dafb'],
+            },
+          ]
+        });
+      }
+    }
+    chart();
+
+  }}, [timeLeft] );
+
+  return(
+      <div className="timer">
+        <div>
+          <span>{timeLeft ? Math.floor((timeLeft / (3600000)) % 24) + ':' + Math.floor((timeLeft / 60000) % 60) + ':' + Math.floor((timeLeft / 1000) % 60) : 'Süreniz Dolmuştur'}</span>
+        </div>
+        <Doughnut data={chartData}  options={{height:'200 px',tooltips:{enabled:false},maintainAspectRatio: true, cutoutPercentage:80, animation:{animateRotate:true}}}/>
+
+      </div>
+  );
+}
+export default RightBar;
